e32270e706d74285e9a3e5642901c45e1b3f4f88
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-10 12:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = 'bob';
 }
 
-let favoriteCustomer;
+let favoriteCustomer = 'claire';
 
 function attemptTwoFavoriteCustomers(){
   let favoriteCustomer;
